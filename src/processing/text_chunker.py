9720ebacd9119b77f5575048dbfcc4e8a4d64d5c
João Saraiva <joaogabrielssaraiva@gmail.com>
2026-03-06 21:50:16 -0300

    feat: Add Pydantic model and text chunking functionality for resume processing

---
+++ src/processing/text_chunker.py
@@ -0,0 +1,55 @@
+from typing import List
+from schemas import ResumeChunk
+
+
+def chunk_text(raw_text: str, file_name: str) -> List[ResumeChunk]:
+    """
+    Slices the resume text based on headers in UPPERCASE letters.
+    """
+    valid_chunks = []
+    
+    # 1. Basic cleaning: removes page breaks from PDF and double spaces
+    clean_text = raw_text.replace('\x0c', '') # Remove page break character
+    
+    # 2. Splits the text line by line for analysis
+    lines = clean_text.split('\n')
+    
+    current_section = "INITIAL_HEADER"
+    current_content = []
+
+    for line in lines:
+        line_stripped = line.strip()
+        if not line_stripped:
+            continue
+
+        # WHEN IS UPPER AND < 45 Chars.
+        if line_stripped.isupper() and len(line_stripped) < 45:
+            
+            if current_content:
+                texto_junto = " ".join(current_content)
+                
+                # Validated Chunk
+                chunk = ResumeChunk(
+                    file_name=file_name,
+                    chunk_id=f"{file_name[:-4]}_{current_section.replace(' ', '_')}",
+                    text_content=f"{current_section}:\n{texto_junto}"
+                )
+                valid_chunks.append(chunk)
+            
+            current_section = line_stripped
+            current_content = []
+        
+        else:
+            current_content.append(line_stripped)
+
+    if current_content:
+        texto_junto = " ".join(current_content)
+        chunk = ResumeChunk(
+            file_name=file_name,
+            chunk_id=f"{file_name[:-4]}_{current_section.replace(' ', '_')}",
+            text_content=f"{current_section}:\n{texto_junto}"
+        )
+        valid_chunks.append(chunk)
+
+
+    return valid_chunks
